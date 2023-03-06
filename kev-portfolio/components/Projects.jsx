bb9c1d9472522b0e44edc90d780099cd11589948
Kevin De La Torre <61712433+sebtheedark@users.noreply.github.com>
2023-03-05 18:33:49 -0800

    working

--- kev-portfolio/components/Projects.jsx
+++ kev-portfolio/components/Projects.jsx
@@ -1,5 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
+import unitedImg from '../img/united_app.png'
+import Link from 'next/link'
+import ProjectItem from './ProjectItem'
+import playGames from '../img/we_play_games.png'
 
 const Projects = () => {
   return (
@@ -8,9 +12,16 @@ const Projects = () => {
               <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
               <h2 className='py-4'>What I've built:</h2>
               <div className='grid md:grid-cols-2 gap-8'>
-                  <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-right from-[#5651e5] to-[#709dff]'>
-                    <Image src='' alt='project 1' />
-                  </div>
+                  <ProjectItem title='Vehicle Inventory'
+                      backgroundImg={unitedImg}
+                      techStack='HTML CSS JS'
+                      projectUrl='/vehicle'
+                  />
+                  <ProjectItem title='Games'
+                      backgroundImg={playGames}
+                      techStack='HTML CSS JS BOOTSTRAP'
+                      projectUrl='/games'
+                  />
               </div>
           </div>
     </div>
